1d799827b6c690cd8e230cfa877bacf71796727b
AJAC2000 <ajcardoza2000@gmail.com>
2023-07-27 17:50:02 -0600

    Added functions for sketchpad operation and colorpicker.

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,24 +1,38 @@
 const sketchpad = document.querySelector('#sketchpad');
+const colorpicker = document.querySelector('#colorpicker');
 
-const createRows = (rows) => {
-    for (let i = 0; i <= rows; i++) {
+let pickedColor = colorpicker.value;
+
+function createRows(rows) {
+    for (let i = 1; i <= rows; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         sketchpad.appendChild(row);
     }
 }
 
-let createColumns = (columns) => {
+function createColumns(columns) {
     const rows = document.querySelectorAll('#sketchpad > .row');
     rows.forEach((row) => {
-        for (let i = 0; i <= columns; i++) {
+        for (let i = 1; i <= columns; i++) {
             const column = document.createElement('div');
             column.classList.add('column');
-            column.innerText = 'Yo!';
             row.appendChild(column);            
         }
     });
 }
 
-createRows(10)
-createColumns(5)
+function makeColorable() {
+    let columns = document.querySelectorAll('div.column');
+    columns.forEach((column) => {
+        column.addEventListener('mousedown', (e) => {
+            e.target.style.background = pickedColor;
+            console.log(e.target)
+        })
+    })
+    console.log(columns)
+}
+
+createRows(16)
+createColumns(16)
+makeColorable()
