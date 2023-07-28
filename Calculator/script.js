b8e7544512fd324dc232e5f1a6fdc10c2efa8650
AJAC2000 <ajcardoza2000@gmail.com>
2023-07-27 20:25:00 -0600

    Added clear, size range and confirm

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,8 +1,13 @@
 const sketchpad = document.querySelector('#sketchpad');
 const colorpicker = document.querySelector('#colorpicker');
+const sizepicker = document.querySelector('#sizepicker');
+const confirmbutton = document.querySelector('#confirmbutton');
 
+// Default values
 let pickedColor = colorpicker.value;
+let sketchpadSize = 16;
 
+// Change color
 function pickColor() {
     colorpicker.addEventListener('input', () => {
         pickedColor = colorpicker.value;
@@ -10,6 +15,23 @@ function pickColor() {
     })
 }
 
+// Change sketchpad size
+function pickSize() {
+    sizepicker.addEventListener('input', () => {
+        sketchpadSize = sizepicker.value
+        console.log(sketchpadSize)
+    })
+}
+
+function changeSize() {
+    confirmbutton.addEventListener('click', () => {
+        clearSketchpad();
+        createRows(sketchpadSize);
+        createColumns(sketchpadSize);
+    })
+}
+
+// Create sketchpad
 function createRows(rows) {
     for (let i = 1; i <= rows; i++) {
         const row = document.createElement('div');
@@ -29,6 +51,7 @@ function createColumns(columns) {
     });
 }
 
+// Color sketchpad tiles
 function makeColorable() {
     let columns = document.querySelectorAll('div.column');
     columns.forEach((column) => {
@@ -58,7 +81,18 @@ function colorTile(event) {
     event.target.style.background = pickedColor;
 }
 
+// Clear sketchpad
+function clearSketchpad() {
+    while (sketchpad.firstChild) {
+        sketchpad.removeChild(sketchpad.lastChild);
+    } ;
+}
+
+
+// Runtime
 pickColor()
+pickSize()
 createRows(16)
 createColumns(16)
 makeColorable()
+changeSize()
