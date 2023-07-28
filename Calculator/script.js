745dfd2a3db7e197f408c8e3888ce1d473d58d35
AJAC2000 <ajcardoza2000@gmail.com>
2023-07-27 19:00:04 -0600

    Added color change and mouse functionality

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,6 +3,13 @@ const colorpicker = document.querySelector('#colorpicker');
 
 let pickedColor = colorpicker.value;
 
+function pickColor() {
+    colorpicker.addEventListener('input', () => {
+        pickedColor = colorpicker.value;
+        console.log(pickedColor)
+    })
+}
+
 function createRows(rows) {
     for (let i = 1; i <= rows; i++) {
         const row = document.createElement('div');
@@ -25,14 +32,33 @@ function createColumns(columns) {
 function makeColorable() {
     let columns = document.querySelectorAll('div.column');
     columns.forEach((column) => {
-        column.addEventListener('mousedown', (e) => {
-            e.target.style.background = pickedColor;
-            console.log(e.target)
+        column.addEventListener('mousedown', () => {
+            addColorTile(columns);
         })
     })
     console.log(columns)
 }
 
+function addColorTile(columns) {
+    columns.forEach((column) => {
+        column.addEventListener('mousemove', colorTile)
+        column.addEventListener('mouseup', () => {
+            removeColorTile(columns);
+        })
+    })
+}
+
+function removeColorTile(columns) {
+    columns.forEach((column) => {
+        column.removeEventListener('mousemove', colorTile)
+    })
+}
+
+function colorTile(event) {
+    event.target.style.background = pickedColor;
+}
+
+pickColor()
 createRows(16)
 createColumns(16)
 makeColorable()
